f582779e04bebff17c498ad6cb795a457103ddd3
shubham <shubhamdv19@gmail.com>
2021-09-13 15:14:36 +0530

    set the background image from unsplash api done

---
+++ script.js
@@ -0,0 +1,8 @@
+const API_KEY = 'zbkD-3Jt4yjUouvEtUCmzuC1NwgKboOBwDnv99wf96k';
+
+//calling the unsplash api
+fetch(`https://api.unsplash.com/photos/random?orientation=landscape&query=nature&client_id=${API_KEY}`)
+.then(res => res.json())
+.then(data => {
+    document.body.style.backgroundImage = `url(${data.urls.full})`
+})
