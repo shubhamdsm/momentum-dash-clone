903c6fa3914de6be04723ee17c931fe5b2024e30
shubham <shubhamdv19@gmail.com>
2021-09-13 15:58:06 +0530

    bitcoin logo styled

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 const API_KEY = 'zbkD-3Jt4yjUouvEtUCmzuC1NwgKboOBwDnv99wf96k';
 
-//calling the unsplash api
-fetch(`https://api.unsplash.com/photos/random?orientation=landscape&query=nature&client_id=${API_KEY}`)
+// calling the unsplash api
+// fetch(`https://api.unsplash.com/photos/random?orientation=landscape&query=nature&client_id=${API_KEY}`)
+// .then(res => res.json())
+// .then(data => {
+//     document.body.style.backgroundImage = `url(${data.urls.full})`
+// })
+// .catch(error => {
+//     console.log('error -----> ',error)
+// })
+
+fetch(`https://api.coingecko.com/api/v3/coins/bitcoin`)
 .then(res => res.json())
 .then(data => {
-    document.body.style.backgroundImage = `url(${data.urls.full})`
-})
-.catch(error => {
-    console.log('error -----> ',error)
+    document.getElementById('bitcoin-name').innerHTML = `
+    <img src = ${data.image.small} alt = 'bitcoin'/>
+    <h1>${data.name}</h1>
+    
+    `
+    document.ge
 })
 
