df9706c03382e0fa8b3b8666c8235057c7c0eb21
shubham <shubhamdv19@gmail.com>
2021-09-13 19:43:12 +0530

    implemented time with Date API

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const API_KEY = 'zbkD-3Jt4yjUouvEtUCmzuC1NwgKboOBwDnv99wf96k';
 const APP_ID = '05c51a69e8891e5ccfa12f1c32542837'
 
-// calling the unsplash api
+
 fetch(`https://api.unsplash.com/photos/random?orientation=landscape&query=nature&client_id=${API_KEY}`)
 .then(res => res.json())
 .then(data => {
+    console.log(data)
     document.body.style.backgroundImage = `url(${data.urls.full})`
+    document.getElementById('author').textContent = `Shot By: ${data.user.name}`
 })
 .catch(error => {
     console.log('error -----> ',error)
@@ -25,6 +27,9 @@ fetch(`https://api.coingecko.com/api/v3/coins/bitcoin`)
     <p> Low: &#8377; ${data.market_data.low_24h.inr} </p>
     `
 })
+.catch(error => {
+    console.log('error -----> ',error)
+})
 
 navigator.geolocation.getCurrentPosition(position =>  {
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=${APP_ID}`)
@@ -35,13 +40,21 @@ navigator.geolocation.getCurrentPosition(position =>  {
        document.getElementById('icon').innerHTML = `
        <img src=${icon} alt='icon'/>`
        document.getElementById('temp').innerHTML = `
-       <h1>${data.main.temp}</h1>
+       <h1>${Math.round(data.main.temp)} &#176;C</h1>
        `
        document.getElementById('city').innerHTML = `
        <h1>${data.name}</h1>
        `
 
    })
-   .catch(err => console.log(err))
+   .catch(error => {
+    console.log('error -----> ',error)
+})
     
 });
+
+function getTime(){
+let date = new Date()
+document.getElementById('time').innerHTML = date.toLocaleTimeString('en-IN',{timeStyle:'short'})}
+
+setInterval(getTime,1000)
